3dce6ef261c39971fc2f562e1287f00170bad0b8
Valentina Gomez Valencia <71842995+valgv@users.noreply.github.com>
2021-12-05 17:10:41 -0500

    fabula, libro, periodico

---
+++ src/main/java/comunicacion/Fabula.java
@@ -0,0 +1,43 @@
+package comunicacion;
+
+public class Fabula extends Escrito {
+	private String ensenanza;
+	private String interpretacion;
+
+	public Fabula(String origen, String titulo, String autor, int paginas, String ensenanza, String interpretacion) {
+		super(origen, titulo, autor, paginas);
+		this.ensenanza=ensenanza;
+		this.interpretacion=interpretacion;
+	}
+	@Override
+	public int palabrasTotales(int palabrasPagina) {
+		// multipliacion de las paginas por el parametro por un factor F=1
+		return getPaginas()*palabrasPagina*1;
+	}
+	@Override
+	public String interpretacion() {
+		return interpretacion;
+	}
+	@Override
+	public String toString() {
+		return  getOrigen() + "\n"
+				+ getTitulo()+ "\n"
+				+ getAutor()+ "\n"
+				+ getPaginas()+ "\n"
+				+ensenanza + "\n"
+				+interpretacion+"\n";
+	}
+	public void setEnsenanza(String ensenanza) {
+		this.ensenanza=ensenanza;
+	}
+	public String getEnsenanza() {
+		return ensenanza;
+	}
+		
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion=interpretacion;
+	}
+	/*public String getInterpretacion() {
+		
+	}*/
+}
---
+++ src/main/java/comunicacion/Periodico.java
@@ -0,0 +1,53 @@
+package comunicacion;
+
+public class Periodico extends Escrito {
+	private String fecha;
+	private String primicia;
+	private String interpretacion;
+
+	public Periodico(String origen, String titulo, String autor, int paginas, String fecha, String primicia, String interpretacion) {
+		super(origen, titulo, autor, paginas);
+		this.fecha=fecha;
+		this.primicia=primicia;
+		this.interpretacion=interpretacion;
+	}
+	@Override
+	public int palabrasTotales(int palabrasPagina) {
+		//f=10
+		return getPaginas()*palabrasPagina*10;
+		
+	}
+	@Override
+	public String interpretacion() {
+		return interpretacion;
+	}
+	@Override
+	public String toString() {
+			return  getOrigen() + "\n"
+					+ getTitulo()+ "\n"
+					+ getAutor()+ "\n"
+					+ getPaginas()+ "\n"
+					+fecha + "\n"
+					+primicia+"\n"
+					+interpretacion+"\n";
+	}
+	public void setFecha(String fecha) {
+		this.fecha=fecha;
+	}
+	public String getFecha() {
+		return fecha;
+	}
+	public void setPrimicia(String primicia) {
+		this.primicia=primicia;
+	}
+	public String getPrimicia() {
+		return fecha;
+	}
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion=interpretacion;
+	}
+	/*public String getInterpretacion() {
+		
+	}*/
+
+}
